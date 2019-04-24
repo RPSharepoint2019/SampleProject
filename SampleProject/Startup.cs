3863a93b8938c46803247f45bbe8610e42a835c3
RPSharepoint2019 <prasadaluka0101@gmail.com>
2019-04-23 21:37:21 -0700

    Sample Commit

---
+++ SampleProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleProject.Startup))]
+namespace SampleProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
